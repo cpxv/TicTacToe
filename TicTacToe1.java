c200b369c04fd0d4d7bf364d16b021baa58874cd
cpxv <121155391+cpxv@users.noreply.github.com>
2022-12-21 20:16:39 +0530

    Add files via upload

---
+++ TicTacToe1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tictactoe1;
+
+import TicTacTes.TicTacToe_s;
+
+/**
+ *
+ * @author Admin
+ */
+public class TicTacToe1 {
+
+    public static void main(String[] args) {
+        new TicTacToe_s().setVisible(true);
+    }
+}
